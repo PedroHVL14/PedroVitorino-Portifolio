579efc106433b94a0b0d09a65725a635256f1460
PedroHVL14 <pedro.vitorino.lima@gmail.com>
2025-04-02 19:43:10 -0300

    Contato

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </p>
             <div className="flex space-x-4">
               <a 
-                href="https://github.com" 
+                href="https://github.com/PedroHVL14" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="bg-gray-800 hover:bg-primary-600 p-2 rounded-full transition-colors"
@@ -24,20 +24,12 @@ const Footer = () => {
                 <Github size={20} />
               </a>
               <a 
-                href="https://linkedin.com" 
+                href="https://www.linkedin.com/in/pedro-vitorino-8b9a0424b/" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="bg-gray-800 hover:bg-primary-600 p-2 rounded-full transition-colors"
               >
                 <Linkedin size={20} />
-              </a>
-              <a 
-                href="https://twitter.com" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="bg-gray-800 hover:bg-primary-600 p-2 rounded-full transition-colors"
-              >
-                <Twitter size={20} />
               </a>
             </div>
           </div>
